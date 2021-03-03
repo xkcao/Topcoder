e4997717d4d16105e161c44702492c3937cadb7c
xiaokang <xiaokang.cao@ge.com>
2021-03-02 18:24:52 -0800

    Substitute: initial add.

---
+++ Substitute/src/com/xiaokang/Main.java
@@ -0,0 +1,9 @@
+package com.xiaokang;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("hello");
+    }
+}
---
+++ Substitute/src/com/xiaokang/Substitute.java
@@ -0,0 +1,7 @@
+package com.xiaokang;
+
+public class Substitute {
+    public int getValue(String key, String code){
+        return 0;
+    }
+}
