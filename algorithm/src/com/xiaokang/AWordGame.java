22393bed2b3877c17a0e56c953e8530045abf837
xiaokang <xiaokang.cao@ge.com>
2021-04-29 21:41:37 -0700

    AWordGame: skip this one.

---
+++ algorithm/src/com/xiaokang/AWordGame.java
@@ -0,0 +1,11 @@
+package com.xiaokang;
+
+public class AWordGame {
+    /*
+    I simply don't like this problem statement.
+     */
+    public  String outcome(String[] wordList){
+
+        return "";
+    }
+}
